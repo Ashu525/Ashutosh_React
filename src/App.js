5837e7149c8d7a89d78d9242498ab6fc4f36fb87
Ashu692 <ashutoshshrivastava692@gmail.com>
2021-08-30 14:40:40 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Menu>
       <Switch>
-        <Route exact path="/themes" component={Home} />
+        <Route exact path="/" component={Themes} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/themes" component={Themes} />
